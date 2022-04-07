be39576dbd2c0adfe5da65feaf49a842366f3e63
Andrej Pedchenko <graduadu@gmail.com>
2022-04-07 19:42:49 +0300

    Added haptics notification

--- Devote/View/NewTaskItemView.swift
+++ Devote/View/NewTaskItemView.swift
@@ -54,6 +54,7 @@ struct NewTaskItemView: View {
                 Button(action: {
                     addItem()
                     playSound(sound: "sound-ding", type: "mp3")
+                    feedback.notificationOccurred(.success)
                 }, label: {
                     Spacer()
                     Text("Save".uppercased())
